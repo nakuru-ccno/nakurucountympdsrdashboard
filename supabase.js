dcf91f134f900e33116dfefe96ce44713b4a509b
nakuru-ccno <nakurucountychiefnursingoffice@gmail.com>
2025-11-15 10:35:36 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+    "https://vypfkihkbldvgoeejtfp.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5cGZraWhrYmxkdmdvZWVqdGZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNDk1MTksImV4cCI6MjA3ODcyNTUxOX0.aePpieODsvkEQSt6rGM695_xWDMv1MOGD25PzErOE-Q"
+);
